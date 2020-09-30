33a8d8459e9c4e4630011a46e033cffd46804584
ALEKHYA ROY <alekhya.roy.3@gmail.com>
2020-09-30 19:59:59 +0530

    Currency Converter

---
+++ Converter.js
@@ -0,0 +1,6 @@
+            async function convert(){
+                var base = document.getElementById('Base').value;
+                var amount =  document.getElementById('Amount').value;
+				result=bas*73.15
+                document.getElementById('Converted').innerText=result
+            }
